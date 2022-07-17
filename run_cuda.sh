339ad4a93a76e353c65273948275f49a4be52564
powcoder代写 <powcoder@163.com>
2022-07-17 18:38:04 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ run_cuda.sh
@@ -0,0 +1,6 @@
+nvcc -std=c++11 -arch=compute_37 -code=sm_37 main.cu cuda_smith_waterman_skeleton.cu -o cuda_smith_waterman
+num_block=13
+num_thread=256
+input=./datasets/1k.in
+echo $input $num_block $num_thread
+./cuda_smith_waterman $input $num_block $num_thread
